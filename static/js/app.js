87ce5e8e563e881e21630ddc7d0af42c23cee92c
mfGWU <68978271+mfGWU@users.noreply.github.com>
2020-10-27 20:55:37 -0400

    Updates to add Filters and use the "if"statement.
    Updates to add Filters and use the "if"statement (11.5.3 and 11.5.4).

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,35 @@ function buildTable(data) {
       );
     });
   }
+
+  function handleClick() {
+      // Need to create a couple of variables to 
+      // hold our date data, both filtered and unfiltered
+      // D3 is look for the #datetime id in the HTML tags
+      // not only to look for where our date values are stored 
+      // on the webpage, but to actually grab that information 
+      // and hold it in the "date" variable.
+    let date = d3.select("#datetime").property("value");
+      
+      // set a default filter and save it to a new variable
+      // tableData is the original data as imported 
+      // from our data.js file
+    let filteredData = tableData;   
+
+    if (date) {
+        // Apply the filter to the table data 
+        // "Show only the rows where the date is equal to the date filter we created above."
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+       // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+  //Listen for the Event 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
