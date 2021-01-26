a370e41f74b64f09f12da541b619969a810edd67
mfGWU <68978271+mfGWU@users.noreply.github.com>
2021-01-25 23:19:22 -0500

    Updated app.js
    Updated app.js

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # UFO
 
-Deliverable 2: A written report on the UFO Analysis (Mia Fullerton)
+## Deliverable 2: A written report on the UFO Analysis (Mia Fullerton)
 
 Initialize your repository with a README, and write your analysis of Deliverable 1.
 
-Overview of Project: 
+## Overview of Project: 
 
 The deliverables of this project is to provide a webpage which lets Dana filter multiple criteria like date, city, state, country, and shape simultaneously. The filtered data is displayed into table dynamically providing an in-dept analysis of UFO sightings. 
 
 
-Results: 
+## Results: 
 
 In the webpage, Dana can either enter each date, city, state, country, and shape criteria or all criteria simultaneously to filter her search.  The JavaScript we wrote will store the text criteria she entered with the associated object.  See image below.
 
@@ -17,6 +17,6 @@ In the webpage, Dana can either enter each date, city, state, country, and shape
 Filter Search Image.
 
 
-Summary
+## Summary
 
-After creating the webpage and dynamic table, the one drawback that I noticed is that the code only allows pull data from a single source.  I recommend to update the script to pull from multiple sources and to keep data hidden until search criteria is entered.
+After creating the webpage and dynamic table, the one drawback that I noticed is that the code only allows pull data from a single source.  Update the script to pull from multiple sources, filtered data is presented to the index page. 
--- static/js/app.js
+++ static/js/app.js
@@ -7,13 +7,11 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
   // First, clear out any existing data
   tbody.html("");
-
   // Next, loop through each object in the data
   // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
-
     // Loop through each field in the dataRow and add
     // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
@@ -22,68 +20,43 @@ function buildTable(data) {
     });
   });
 }
-
 // 1. Create a variable to keep track of all the filters as an object.
-
 var filters = {};
-
 // 3. Use this function to update the filters. 
-// Event listener that hold all the user input.
 function updateFilters() {
-
     // 4a. Save the element that was changed as a variable.
-    let changeFilter = d3.select(this);
-    //console.log(changeFilter)
-
+    let changeElement = d3.select(this);
+    console.log(changeElement)
     // 4b. Save the value that was changed as a variable.
-    let elementValue = changeFilter.property("value");
+    let elementValue = changeElement.property("value");
     console.log(elementValue);
-
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterId = changeFilter.attr("id");
+    let filterId = changeElement.attr("id");
     console.log(filterId);
-  
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
       filters[filterId] = elementValue;
     }
     else {
-      delete filters[filterId]
+      delete filters[filterId];
     }
-  
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
-  
   }
-  
   // 7. Use this function to filter the table when data is entered.
-function filterTable() {
-  
+  function filterTable() {
     // 8. Set the filtered data to the tableData.
-    let filters = tableData;
-  
+    let filteredData = tableData;
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    filters.forEach(function(item) {
-      //console.log(item.textContent)
-      txtValue = elementValue;
-      if(item.txtValue.toLowerCase().indexOf(filters) > -1){
-          //item.closest("li").style.display = "";
-          item.style.display = "";
-      }  else {
-        //item.closest("li").style.display = "none";
-          item.style.display = "none";
-      }
-    })
-    
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filters);
-  
+    buildTable(filteredData);
   }
-  
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
-  
   // Build the table when the page loads
   buildTable(tableData);
